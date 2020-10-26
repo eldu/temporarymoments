b727acd681206670f9f0f448bd58a07c6dfad2a5
Ellen <shi.ellenduong@gmail.com>
2020-10-25 23:15:26 -0400

    Update images

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -1,14 +1,23 @@
 # Instructions
 
+## Photo Specifications
+
+Final Photo for display
+- Fit Dimensions: 2560 x 1440 px
+- Aspect Ratio: 16:9
+- File size recommended <= 500 kb. Max is 20 mb.
+- .png or .jpg
+
 ## Updating the photo database
 Basically, I run a script that queries Google Drive for the photos. Then it saves metadata about those photos into 
 [photos.json](_data/photos.json). This data is then used to build the website.
 
 1. Add photos to the Google Drive folder for temporary moments
-1. Run `python get_photos.py` with a credentials file. It creates a `photos.json` file.
+1. Run `python update_photos.py` with a credentials file. It creates a `photos.json` file.
 1. Build
-  - `npm run build` or `bundle exec jekyll build`
+    - `npm run build` or `bundle exec jekyll build`
 1. Test locally
-  - `npm start` or `bundle exec jekyll serve`
+    - `npm start` or `bundle exec jekyll serve`
+1. Push to Master
 1. Deploy
-  - `npm run deploy`
+    - `npm run deploy`
--- update_photos.py
+++ update_photos.py
@@ -1,3 +1,10 @@
+"""
+Improvements to make on updating photos
+
+Example Run
+python update_photos.py --credentials_file "C:\\Users\\Ellen\\Credentials\\temporarymoments_client_secret.json"
+"""
+import argparse
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -6,13 +13,19 @@ from google.auth.transport.requests import Request
 import json
 
 TEMPORARYMOMENTS_FOLDER_ID = "1m1SBar05i6ov59CPfz_CsrGA-iwiSzxb"
-SEATTLE_FOLDER_ID = "1vjDZYfEf0pfTu-_qk5zWf6p3kPTvjMpS"
+SEATTLE_FOLDER_ID = "1ASIRGe59CgDIpztCJGUEtQOkGKNpVKSw"
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Update photos")
+    parser.add_argument('--credentials_file', dest='credentials_file', required=True,
+                        help="path to the credentials file")
+    args = parser.parse_args()
+    credentials_file = args.credentials_file
+
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -25,8 +38,7 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials/google_drive_client_secret_desktop.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
@@ -36,7 +48,8 @@ def main():
 
     kwargs = {
         "q": "'{}' in parents".format(SEATTLE_FOLDER_ID),
-        "fields": "nextPageToken,files(id,parents,name,webContentLink,description)",
+        "fields": "nextPageToken,files",
+        "orderBy": "name"
     }
 
     # Call the Drive v3 API
